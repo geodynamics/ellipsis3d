fe9b1d37161e31cf73028892a942a194507de88b
Leif Strand <leif@geodynamics.org>
2006-04-04 01:26:02 +0000

    Cleaning.

--- Source_code/_options_variables_files.h
+++
@@ -1,4 +0,0 @@
-/* This file generated automatically - edits will not be preserved */
-#include "/home/boo/cig/ellipsis3d/Source_code/advection_global_variables.h" 
-#include "/home/boo/cig/ellipsis3d/Source_code/convection_global_variables.h" 
-#include "/home/boo/cig/ellipsis3d/Source_code/tracer_global_variables.h" 
